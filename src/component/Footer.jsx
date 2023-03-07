07681e7c1df42449c91c8cef9de94be52d7509c4
Amar <108946547+AmarVcode@users.noreply.github.com>
2023-03-07 18:56:33 +0530

    updated

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -2,7 +2,30 @@ import React from 'react'
 
 const footer = () => {
   return (
-    <div>footer</div>
+    <footer>
+      <div className="logo">
+      <img src="https://www.sciastra.com/Assets/Images/new_logo_sci(1).png" alt="" />
+    </div>
+      <div>
+
+SciAstra
+
+      </div>
+      <h2>
+      © SciAstra
+      </h2>
+      <a href="#">Home</a>
+      <a href="#">Our Courses</a>
+      <a href="#">Blog</a>
+      <a href="#">Free Materials</a>
+      <a href="#">Our Team</a>
+      <a href="#">Contact Us</a>
+  
+  
+ 
+  
+  
+    </footer>
   )
 }
 
--- src/component/maincontent/Main.jsx
+++ src/component/maincontent/Main.jsx
@@ -81,6 +81,9 @@ A good teacher is like a candle, it consumes itself to light the way for others
     <h5>
     AIR-1 (IAT) AIR-15 (NEST)
     </h5>
+    <a href="#">
+      CMI
+    </a>
   </div>
 </div>
 <div className='cardinfo'>
@@ -99,6 +102,9 @@ Dhirtiraj Bastav
     <h5>
     Rank-1 (Assam board)
     </h5>
+    <a href="#">
+    IISER Pune 
+    </a>
   </div>
 </div>
 </div>
@@ -108,13 +114,246 @@ Dhirtiraj Bastav
 
 
 
+<div className="block">
+  <div>
+    <h2>
+      30000+
+    </h2>
+    <p>
+Community
+    </p>
+  </div>
+  <div>
+    <h2>
+      3000+
+    </h2>
+    <p>
+Live Classes
+    </p>
+  </div>
+  <div>
+    <h2>
+      500+
+    </h2>
+    <p>
+    Mock Test
+    </p>
+  </div>
+  <div>
+    <h2>
+      400+
+    </h2>
+    <p>
+Selections
+    </p>
+  </div>
+</div>
+
+
+<div className="box2">
+  <div>
+<h2 className="head exc">
+Live and Recorded Classess
+</h2>
+<p>
+Interaction sessions, Doubt classes, Discussions all in one App
+</p>
+  </div>
+  <div>
+  <video src="https://sciastra.com/Assets/Videos/Master_comp_1.webm" autoPlay={true} muted="" loop=""></video>
+  </div>
+  
+</div>
+
+
+<div className="box2">
+  <div>
+  <img src="https://cdn.sciastra.com/gifs/analysis.gif" alt="" />
+  </div>
+  <div>
+<h2 className="head exc">
+Latest Pattern Mock Test 
+</h2>
+<button className='btn1'>
+Mock Tests
+</button>
+<button className='btn1 btn2'>
+Test Analysis
+</button>
+  </div>
+  
+</div>
+
+
+<div className="box2">
+  <div>
+<h2 className="head exc">
+Instant chat with mentors
+</h2>
+<p>
+Personal and emotional support for students
+</p>
+  </div>
+  <div>
+  <img src="https://files.sciastra.com/chatsub.gif" alt="" />
+  </div>
+  
+</div>
+
+
+<div className="box3">
+  <p>
+  Courses
+  </p>
+  <h2 className='head exc'>
+  Full Preparation
+  </h2>
+  <p>
+  You won't need anything outside the course
+  </p>
+</div>
 
 
 
 
+<div className="box2">
+  <div className='mini'>
+    <div className='white'>
+      <h5>
+      ISI + CMI Crash Course 2023
+      </h5>
+    </div>
+    <div className='flexmini'>
+<h5>
+  ISI
+</h5>
+<button className='btnred'>
+  62 % Off 
+</button>
+<h3 className='striked'>
+  ₹ 7999
+</h3>
+<h3 className='colored'>
+  ₹ 2999
+</h3>
+<a href="#">
+  View Details  
+
+</a>
+<button className='btnmain'>
+  Buy Now
+</button>
+    </div>
+  </div>
+  <div className='mini'>
+    <div className='white'>
+      <h5>
+    Spartan 2.0 (2023)
+      </h5>
+    </div>
+    <div className='flexmini'>
+<h5>
+  ISI
+</h5>
+<button className='btnred'>
+  62 % Off 
+</button>
+<h3 className='striked'>
+  ₹ 7999
+</h3>
+<h3 className='colored'>
+  ₹ 2999
+</h3>
+<a href="#">
+  View Details  
+
+</a>
+<button className='btnmain'>
+  Buy Now
+</button>
+    </div>
+  </div>
+  <div className='mini'>
+    <div className='white'>
+    <h5>
+    ISI & CMI Full Year Course
+    </h5>
+    </div>
+    <div className='flexmini'>
+<h5>
+  ISI
+</h5>
+<button className='btnred'>
+  62 % Off 
+</button>
+<h3 className='striked'>
+  ₹ 7999
+</h3>
+<h3 className='colored'>
+  ₹ 2999
+</h3>
+<a href="#">
+  View Details  
+
+</a>
+<button className='btnmain'>
+  Buy Now
+</button>
+    </div>
+  </div>
+
+  <div className="white">
+  View All Courses <em>  ----<u>Here</u></em>
+  </div>
+</div>
 
 
 
+<h2 className='head exc'>
+  Important Update
+</h2>
+
+
+<div className="box2">
+  <div>
+    <div className="white"></div>
+  <iframe src="https://www.sciastra.com/newssmedia.php" id="newsframe">
+                                </iframe>
+  </div>
+  <div>
+<h2 className="head exc">
+Download
+</h2>
+<p>
+Our app SciAstra
+</p>
+<button className='btn1'>
+Goggle Play
+</button>
+<button className='btn1'>
+App Store
+</button>
+  </div>
+  
+</div>
+
+
+
+
+<div className="box2">
+  <div>
+<h2 className="head exc">
+Join the community!
+</h2>
+<p>
+Get your SciAstra T-shirt to become a part of the cult of science lovers.
+</p>
+  </div>
+  <div>
+  <img src="https://files.sciastra.com/Design_3-removebg-preview.png" alt="" />
+  </div>
+  
+</div>
     </div>
   )
 }
